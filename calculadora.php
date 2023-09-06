d5ac81b4b9f57fa3a4b7a3666a30ddf480823abe
Cristian9601 <73319252+Cristian9601@users.noreply.github.com>
2023-09-05 19:04:08 -0500

    Add files via upload

--- calculadora.php
+++ calculadora.php
@@ -1,10 +1,13 @@
+<?php
+require 'nusoap.php'
+?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Calculadora SOAP</title>
+    <title>CALCULADORA</title>
 </head>
 <body>
-    <h2>Calculadora SOAP</h2>
+    <h2>CALCULADORA</h2>
     <form method="POST" action="resultado.php">
         Número 1: <input type="text" name="num1"><br>
         Número 2: <input type="text" name="num2"><br>
--- servidorsoap.php
+++ servidorsoap.php
@@ -1,4 +1,5 @@
 <?php
+require "nusoap.php";
 class Calculadora {
     public function calcular($num1, $num2, $operacion) {
         switch ($operacion) {
@@ -15,9 +16,13 @@ class Calculadora {
         }
     }
 }
-
-$server = new SoapServer('calculadorawsdl.wsdl');
-$server->setClass('Calculadora');
-$server->handle();
+$server = new nusoap_server();
+$server->configureWSDL("Soap Demo","urn:soapdemo");
+$server->register(
+    "Calculadora.calcular",
+    array("numero1"=> "xsd:float","numero2"=>"xsd:float","operacion"=>"xsd:string"),
+    array("return"=>"xsd:float")
+);
+$server->service(file_get_contents("php://input"))
 ?>
 
